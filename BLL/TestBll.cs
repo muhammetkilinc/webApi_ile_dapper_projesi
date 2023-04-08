7007cc7fb11f5ee4210149c279a57904a4859300
muhammetkilinc <doktordariloit@gmail.com>
2023-04-08 22:51:23 +0300

    güncelleme 2

---
+++ BLL/TestBll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class TestBll
+    {
+        public int Topla(int sayi1, int sayi2) => sayi1 + sayi2;
+        public int Topla(int[] sayiListesi) => sayiListesi.Sum();
+    }
+}
